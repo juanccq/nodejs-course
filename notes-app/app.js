a138db1ec4424f7d5dc47785afdf256750438a35
Juan Carlos Choque Quispe <juan.c.c.q@gmail.com>
2022-05-01 12:41:58 -0400

    added node arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,31 @@
+// const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js')
 
-console.log(getNotes());
+yargs.version('1.1.0');
+
+yargs.command('add','Add a new note', {},function () {console.log('Adding a note')}).help().argv;
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function () {
+        console.log('Removing a note');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log('Listing notes');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a new note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+});
