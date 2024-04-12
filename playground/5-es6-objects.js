951f5c1bc137eac78358a63c6bee9edffc6a9fc3
Juan Choque <juan.choque@argano.com>
2024-04-12 19:31:42 -0400

    Added request with native node api

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,31 @@
+// Object property shorthand
+
+const name = 'Juan';
+const userAge = 41;
+
+const user = {
+    name,
+    age: userAge,
+    location: 'La Paz'
+};
+
+console.log(user);
+
+// Object destructuring
+const product = {
+    label: 'Red Notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+const {label:productLabel, stock, rating = 5} = product;
+console.log(productLabel);
+console.log(stock);
+console.log(rating);
+
+const transaction = (type, { label, stock }) => {
+    console.log(`Transaction type: ${type}`);
+}
+
+transaction('order', product);
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,34 +1,24 @@
-const request = require('postman-request');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const url = 'http://api.weatherstack.com/current?access_key=74e8589b476cdb3ac89b554bb34ab219&query=-16.4850751,-68.1789251';
+const address = process.argv[2];
 
-request({ url:url }, (error, response) => {
-    if( error ) {
-        console.log('Unable to connect to weather service');
-    }
-    else if( response.body.error ) {
-        console.log('Unable to find location');
-    }
-    else {
-        const data = JSON.parse(response.body);
-        console.log(data.current);    
-    }
-})
-
-// Geocoding
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVhbmNjcSIsImEiOiJjazFjcTJwbnUwMDh6M21wNnVjMTYwYmN4In0.pdTykSGaN8iGuE5r1uYiTQ&limit-1';
-
-request({ url:geoUrl, json: true }, (error, response) => {
-    if( error ) {
-        console.log('Unable to connect to geocoding service');
-    }
-    else if( response.body.features.length === 0 ) {
-        console.log('Unable to find coordinates');
-    }
-    else {
-        const lat = response.body.features[0].center[1];
-        const long = response.body.features[0].center[0];
-
-        console.log(lat, long);
-    }
-})
+if(!address) {
+    console.log('Please provide an address');
+}
+else {
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if(error) {
+            return console.log(error);
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error);
+            }
+    
+            console.log(location);
+            console.log(forecastData);
+        });
+    })
+}
